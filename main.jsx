917b347ded7f7fb603662c1fd9386288d86f51e2
tylermuse <tylermuse@gmail.com>
2025-09-03 17:27:07 -0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import NobiLanding from "./App.jsx";
+import "./index.css";
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+createRoot(document.getElementById("root")).render(<NobiLanding />);
+

38570976823f5d30613408253ee2ce8b0359d5aa
tylermuse <tylermuse@gmail.com>
2025-09-07 22:29:46 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
-import NobiLanding from "./App.jsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";   // ← this line is required
 
-createRoot(document.getElementById("root")).render(<NobiLanding />);
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

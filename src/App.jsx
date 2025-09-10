dcad38d7443c80540e3654f35f27172babfb9594
tylermuse <tylermuse@gmail.com>
2025-09-09 21:18:46 -0500

    Change from brands to logos

--- src/App.jsx
+++ src/App.jsx
@@ -592,11 +592,11 @@ function Logos() {
 
         {/* Equal-width columns so spacing is perfectly even */}
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 items-center gap-x-10 sm:gap-x-12 lg:gap-x-16 gap-y-8">
-          <BrandLogo src="/media/brands/lucchese.svg"  alt="Lucchese" />
-          <BrandLogo src="/media/brands/faherty.svg"   alt="Faherty" />
-          <BrandLogo src="/media/brands/untuckit.svg"  alt="UNTUCKit" />
-          <BrandLogo src="/media/brands/stbernard.svg" alt="St. Bernard" />
-          <BrandLogo src="/media/brands/kilte.svg"     alt="Kilte" />
+          <BrandLogo src="/media/logos/lucchese.svg"  alt="Lucchese" />
+          <BrandLogo src="/media/logos/faherty.svg"   alt="Faherty" />
+          <BrandLogo src="/media/logos/untuckit.svg"  alt="UNTUCKit" />
+          <BrandLogo src="/media/logos/stbernard.svg" alt="St. Bernard" />
+          <BrandLogo src="/media/logos/kilte.svg"     alt="Kilte" />
         </div>
       </div>
     </section>

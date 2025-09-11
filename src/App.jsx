c082662f99118d59eb78736306dfded7f354d8ae
tylermuse <tylermuse@gmail.com>
2025-09-11 11:30:45 -0500

    Changed 3rd bullet

--- src/App.jsx
+++ src/App.jsx
@@ -776,7 +776,7 @@ function Hero({ onOpenForm, onOpenVideo }) {
               </div>
               <div className="flex items-center gap-2">
                 <CheckCircle2 className="h-4 w-4 text-fuchsia-600" />
-                A/B testing & reporting
+                Insights reporting
               </div>
             </div>
           </div>
